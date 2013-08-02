3975ccdfc38f43d9961cd0170769d53a47facb63
Alex Olivier <alex@alexolivier.me>
2013-08-02 13:31:54 +0100

    run.js

---
+++ run.js
@@ -0,0 +1,3 @@
+'use strict';
+var automedia = require('./lib/automedia.js');
+automedia.awesome('http://www.heyuguys.co.uk/lone-survivor-trailer-poster-images/','.entry-content iframe[src^="http://www.youtube.com/embed"]');
